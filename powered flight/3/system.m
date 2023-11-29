62b897b53c97272c325569c8255a863b93a858a3
Snehit Gupta <snehit.g@research.iiit.ac.in>
2023-11-29 06:04:16 +0530

    matlab

--- powered flight/3/system.m
+++
@@ -1,7 +0,0 @@
-flight_data = readtable('data.csv');
-thr = flight_data(:,"rc_thr").rc_thr;
-acc_X = flight_data(:, "acc_X").acc_X;
-Ts = 0.001;
-order = 4;
-data = iddata(acc_X, thr, Ts);
-tf = tfest(data, order);
---
+++ system.m
@@ -0,0 +1,23 @@
+flight_data = readtable('powered flight/3/data.csv');
+rc_ele = flight_data(:,"rc_ele").rc_ele;
+gyro_pitch = flight_data(:, "gyro_pitch").gyro_pitch;
+Ts = 0.001;
+order = 4;
+train_data = iddata(gyro_pitch, rc_ele, Ts);
+tf = tfest(train_data, order);
+figure;
+compare(data,tf);
+ylabel('pitch (gyro)')
+xlabel('time')
+title('train data')
+legend('actual', 'predicted')
+flight_data_2 = readtable('powered flight/4/data.csv');
+rc_ele_2 = flight_data_2(:,"rc_ele").rc_ele;
+gyro_pitch_2 = flight_data_2(:, "gyro_pitch").gyro_pitch;
+val_data = iddata(gyro_pitch_2, rc_ele_2, Ts);
+figure;
+compare(val_data, tf);
+ylabel('pitch (gyro)')
+xlabel('time')
+title('val data')
+legend('actual', 'predicted')
